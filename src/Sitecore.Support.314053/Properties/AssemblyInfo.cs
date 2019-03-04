9d81fef5c9b70a5ad09f177b6d7eeb2835e1d60e
mao <mao@sitecore.net>
2019-03-04 15:09:41 +0200

    Initial commit

---
+++ src/Sitecore.Support.314053/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.314053")]
+[assembly: AssemblyProduct("Sitecore.Support.314053")]
+[assembly: ComVisible(false)]
